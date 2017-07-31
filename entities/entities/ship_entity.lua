52395dec9021ede0aeccbe91b490bc013a4420d8
Thermadyle <JonasNotanon@gmail.com>
2017-08-01 00:27:12 +0200

    remove if under water

--- entities/entities/ship_entity.lua
+++ entities/entities/ship_entity.lua
@@ -3,9 +3,16 @@ ENT.Base = "base_anim"
 
 function ENT:Think()
     local divide = ents.FindByClass("ships_area_divide")[1]
-    if SERVER and IsValid(divide) then
-        if (divide:GetPos().z + self:BoundingRadius()) >= self:GetPos().z then
+    if SERVER then
+        -- remove if below area divider
+        if IsValid(divide) and (divide:GetPos().z + self:BoundingRadius()) >= self:GetPos().z then
             self:Remove()
+        end
+        -- remove if deep into the water
+        if IsValid(self.ship) then
+            if self.ship:GetEntityWaterLevel(self) >= 3.25 then
+                self:Remove()
+            end
         end
     end
 end
